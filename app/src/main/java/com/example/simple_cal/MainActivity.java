9bfd4f5948e9b60d819ac72f7f6e1fc8b8e70474
SiriwardanaT <74727810+SiriwardanaT@users.noreply.github.com>
2021-01-24 19:32:25 +0530

    add intent in main activity

--- app/src/main/java/com/example/simple_cal/MainActivity.java
+++ app/src/main/java/com/example/simple_cal/MainActivity.java
@@ -14,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button button = findViewById(R.id.actone_button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Activity02.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
